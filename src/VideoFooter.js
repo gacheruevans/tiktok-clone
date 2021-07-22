cc9c5c7c722c9364ff22ce02cc97dfe541e80ade
Evans Gacheru <evans.gacheru@andela.com>
2021-07-22 22:45:26 +0300

    video footer component

---
+++ src/VideoFooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoFooter = () => {
+    return (
+        <div>
+            <h2> I am a footer</h2>
+        </div>
+    );
+}
+
+export default VideoFooter;
